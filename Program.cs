69acad1cb582964ce82f07e96a5142824bc5459e
mast3r <thomasmollerjensen@live.dk>
2023-10-27 12:49:36 +0200

    inital commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using MQTTnet;
+using MQTTnet.Server;
+
+
+public class Program {
+
+    public static void Main(String[] args){
+        // defining options:
+        var options = new MqttServerOptionsBuilder()
+            .WithDefaultEndpoint()
+            .WithDefaultEndpointPort(4000)
+            .Build();
+
+        var server = new MqttFactory().CreateMqttServer(options);
+
+        server.StartAsync().GetAwaiter().GetResult();
+
+        Console.Write("end? > ");
+        Console.ReadLine();
+    }
+
+    public static void OnNewConnection(){
+        
+    }
+
+    public static void OnNewMessage(){
+
+    }
+}
